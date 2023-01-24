c5de94d4c02cd0026555ddb09cbb13e2d9486599
kushurox <kushalsai2449@gmail.com>
2023-01-24 19:13:37 +0530

    added AI functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Project started on 19th of January 2023
 """
 
 import math
+import random
 import wave
 
 import kivymd.uix.button
@@ -43,6 +44,65 @@ RATE = 44100
 p = pyaudio.PyAudio()
 
 sr = speech_recognition.Recognizer()
+
+
+jokes = [
+    """What’s the best thing about Switzerland?
+I don’t know, but the flag is a big plus.""",
+    """Did you hear about the mathematician who’s afraid of negative numbers?
+He’ll stop at nothing to avoid them.""",
+    """I saw Usain Bolt sprinting around the track shouting, "Why did the chicken cross the road?" It was a running joke.
+""",
+    """You're not completely useless. You can always serve as a bad example.""",
+    """I was wondering why the ball was getting bigger, then it hit me."""
+]
+
+
+random_facts = [
+    """Fern collecting was such a hot fad in the Victorian era that it even had a name: pteridomania.""",
+    """Mr. Rogers always mentioned out loud that he was feeding his fish because a young blind viewer once asked him 
+    to do so. She wanted to know the fish were OK.""",
+    """Those tiny "seeds" on the outside of a strawberry aren't seeds--those are actually little tiny fruits!""",
+    """When your 'inner voice' speaks in your head, it triggers small muscle movements in your larynx.""",
+    """Mark Twain was born in 1835, a year in which Halley’s comet was visible from Earth. In 1909, he said, 
+    'I came in with Halley’s comet...and I expect to go out with it.” When he died on April 21, 1910, the comet was 
+    again visible in the night sky.""",
+    """National Donut Day originally started as a way to honor Salvation Army volunteers who served donuts to 
+    soldiers in WWI."""
+]
+
+
+def random_joke(*args):
+    engine.say(random.choice(jokes))
+    engine.runAndWait()
+
+
+def random_fact(*args):
+    engine.say(random.choice(random_facts))
+    engine.runAndWait()
+
+
+def introduction(*args):
+    intro = "Hello! I am an AI i guess. I can Help you with trivial tasks such as setting timers or saying random " \
+            "jokes/facts. I am a very minimalistic Bot as i was meant only for this sem's mini project"
+    engine.say(intro)
+    engine.runAndWait()
+
+
+def timer(*args):
+    engine.say("Since my hearing abilities are really tragic, i suggest you use your phone")
+    engine.runAndWait()
+
+
+intent_map = {
+    "joke": random_joke,
+    "fact": random_fact,
+    "timer": timer,
+    "intro": introduction
+}
+
+
+
 
 
 class AIFx(MDGridLayout):
@@ -118,7 +178,7 @@ class AIFx(MDGridLayout):
             # print(len(data))
             rms = math.sqrt(abs(pow(data, 2).sum() / len(data)))
             norm_rms = (rms / 100) + 2  # offsetting normalized rms by 2 for visual reasons
-            # print(rms, norm_rms)
+            print(rms, norm_rms)
             self.updater(norm_rms)
 
         stream.stop_stream()
@@ -129,6 +189,9 @@ class AIFx(MDGridLayout):
         self.change_voice_line(text)
         engine.say(text)
         engine.runAndWait()  # Fortunately this code was ran in a different thread so it doesn't freeze the whole prog
+
+        intent = "joke"
+        intent_map[intent]()
 
         del d
 
